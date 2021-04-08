31b54be7c1d21d7cbfe78fe62e66e7441cd0f867
Aakash <aakashsrivastava008@gmail.com>
2021-04-08 16:44:40 +0530

    delete task functionallity  added

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -16,3 +16,20 @@ module.exports.createTask = function(req, res) {
 
     return res.redirect('back');
 }
+
+module.exports.deleteTask = function(req, res) {
+
+    console.log(req.query);
+
+    let des = req.query.description;
+    let dat = req.query.date;
+    let cat = req.query.category;
+
+    let indexToBeDeleted = tasksArray.findIndex((data) => {
+        return (data.description == des);
+    });
+
+    tasksArray.splice(indexToBeDeleted, 1);
+
+    return res.redirect('back');
+}
--- routers/index.js
+++ routers/index.js
@@ -8,5 +8,6 @@ const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.home);
 router.post('/create-task', homeController.createTask);
+router.get('/delete-task/', homeController.deleteTask);
 
 module.exports = router;
