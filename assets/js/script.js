52c876d9924b238f471a550cdfb1b6168bc844ad
Aakash <aakashsrivastava008@gmail.com>
2021-04-13 21:06:36 +0530

    Copyright mark and text added. Version 1 of app ready to roll out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
-var checkBox = document.getElementsByClassName('myCheck');
+var checkBoxArray = document.getElementsByClassName('myCheck');
 var delete_btn = document.getElementById('delete-btn');
+var tasks = document.getElementsByClassName('taskBar');
 
-function checkSelected(checkBox){
+delete_btn.style.backgroundColor = "grey"
 
-    if(checkBox.checked){
+function checkSelected(){
+
+    let checkedCount = 0;
+
+    for(let i=0; i<checkBoxArray.length; i++){
+        if(checkBoxArray[i].checked){
+            tasks[i].style.textDecoration = "line-through";
+            checkedCount++;
+        }
+
+        else{
+            tasks[i].style.textDecoration = "none";
+        }
+    }
+
+    if(checkedCount == 0){
+        delete_btn.disabled = true;
+        delete_btn.style.backgroundColor = "grey";
+    }
+
+    else{
         delete_btn.disabled = false;
+        delete_btn.style.backgroundColor = "red";
     }
 }
